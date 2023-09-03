ce696c8acc26e49f6bea712788be636cba5a93fc
Tesheena <singh.tesheena27@gmail.com>
2023-09-02 19:33:39 -0500

    x axis

--- user_easy_stats_tsngh.py
+++ user_easy_stats_tsngh.py
@@ -19,3 +19,31 @@ import sys
 # univariant data (one variable, many readings)
 
 age_data = [19, 23, 63, 22, 19, 25, 35, 40, 20, 6 ]
+
+logger.info("age_data = " + str(age_data))
+
+# Descriptive: Averages and measures of central tendency
+
+mean = statistics.mean(age_data)
+median = statistics.median(age_data)
+mode = statistics.mode(age_data)
+
+# log use variable colon formatting to avoid unnecessary digits (e.g. .2f)
+
+logger.info(f"mean   = {mean:.2f}")
+logger.info(f"median = {median:.2f}")
+logger.info(f"mode   = {mode:.2f}")
+
+# Descriptive: Measures of spread
+
+var = statistics.variance(age_data)
+stdev = statistics.stdev(age_data)
+lowest = min(age_data)
+highest = max(age_data)
+
+# TODO: change to f-strings and display 2 decimal places (like we did above)
+logger.info(f"var    = {var: .2f}")
+logger.info(f"stdev  = {stdev: .2f}")
+logger.info(f"lowest = {lowest: .2f}")
+logger.info(f"highest= {highest: .2f}")
+
