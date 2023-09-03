1e975fc8d2d7b749026aa8a1d4aa44e52bc39c37
Tesheena <singh.tesheena27@gmail.com>
2023-09-02 19:22:13 -0500

    inital layout

---
+++ user_easy_stats_tsngh.py
@@ -0,0 +1,19 @@
+"""
+Tesheen Singh, September 2, 2023 Week 2 - Project 2
+
+Purpose: Illustrate the built-in statistics module. """
+
+# Use this handy logger to document your work automatically
+
+# import setup_logger function from instructor-generated module
+from util_logger import setup_logger
+
+# setup the logger using the current file name (a built-in variable)
+logger, logname = setup_logger(__file__)
+
+# Import from Python Standard Library
+
+import statistics
+import sys
+
+# univariant data (one variable, many readings)
